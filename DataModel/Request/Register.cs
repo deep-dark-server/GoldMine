e5f21b5d09a51c77396f1e05c36ebfeb70e1bf81
LemonTeaDev <jinuxc@gmail.com>
2015-09-29 19:09:13 +0900

    Add lib project for data models

---
+++ DataModel/Request/Register.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace GoldMine.DataModel.Request
+{
+    [DataContract]
+    public enum ProtocolType
+    {
+        [EnumMember]
+        Http = 0,
+
+        [EnumMember]
+        Binary,
+
+        [EnumMember]
+        Both
+    }
+}
