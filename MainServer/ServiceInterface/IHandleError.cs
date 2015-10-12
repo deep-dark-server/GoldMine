4cdc1d11e7d0ec3094da97fdc057be9828239f7e
LemonTeaDev <jinuxc@gmail.com>
2015-10-12 23:52:15 +0900

    Add Error Handling Interface

---
+++ MainServer/ServiceInterface/IHandleError.cs
@@ -0,0 +1,10 @@
+﻿using GoldMine.DataModel.Response;
+
+namespace GoldMine.MainServer
+{
+    public interface IHandleError
+    {
+        ResponseError RespondOnException(System.Exception e);
+        ResponseError RespondOnError(ErrorCode code);
+    }
+}
