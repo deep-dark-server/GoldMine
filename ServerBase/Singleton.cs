f4ab45e195d878c8dde12a2c26ab909a9d31327d
LemonTeaDev <jinuxc@gmail.com>
2015-10-17 17:20:54 +0900

    Common library for server related projects

---
+++ ServerBase/Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ServerBase
+{
+    public abstract class Singleton<T>
+    {
+        private static Lazy<T> instance = null;
+
+        public static bool SetInitFunc(Func<T> func)
+        {
+            if (instance == null)
+            {
+                instance = new Lazy<T>(func);
+                return true;
+            }
+            else
+                return false;
+        }
+
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Lazy<T>();
+                return instance.Value;
+            }
+        }
+    }
+}
