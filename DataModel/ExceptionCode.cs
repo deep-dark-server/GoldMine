66930dcc20d6ef2cb0b4a575566c18a2aefe8d30
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2015-11-08 12:38:48 +0900

    remove custom exception type with http status code, implementd method to translate exceptions into code instead

---
+++ DataModel/ExceptionCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace GoldMine.DataModel
+{
+    public static class ExceptionCode
+    {
+        private const int DefaultErrorCode = 500;
+
+        public static int GetExceptionCode(this Exception ex)
+        {
+            if (ex is ExternalException)
+                return (ex as ExternalException).ErrorCode;
+
+            return DefaultErrorCode;
+        }
+    }
+}
