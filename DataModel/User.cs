4a827c6aaa943970c1980d24638105ded376aa7b
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2015-11-22 23:02:19 +0900

    Add persistence model for User table

---
+++ DataModel/User.cs
@@ -0,0 +1,13 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace GoldMine.DataModel
+{
+    [DynamoDBTable("User")]
+    public class User
+    {
+        [DynamoDBHashKey]
+        public int id { get; set; }
+        public string server_host { get; set; }
+        public byte protocol { get; set; }
+    }
+}
