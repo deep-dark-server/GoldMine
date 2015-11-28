b33378058359dbc12d7d61a228c256be1e9575c9
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2015-11-28 16:52:39 +0900

    OperationTool: ConsoleKey-Command Mapping and Dispatching

--- OperationTool/Program.cs
+++ OperationTool/Program.cs
@@ -1,37 +1,80 @@
-﻿using System;
+﻿using GoldMine.ServerBase.Init;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace GoldMine.OperationTool
 {
     public class Program
     {
+        public static bool IsInteractive
+        {
+            get; private set;
+        }
+        private bool isExit = false;
+        private Dictionary<ConsoleKey, Command> commands = new Dictionary<ConsoleKey, Command>();
+
         private static void Main(string[] args)
         {
+            PostAppInit.Do();
             Program pg = new Program();
+            pg.LoadCommands();
             pg.Main();
         }
 
-        private bool isExit = false;
-        private Dictionary<ConsoleKey, Command> commands;
-
         private void Main()
         {
+            IsInteractive = true;
             ConsoleKeyInfo keyInfo;
             while (!isExit)
             {
+                PrintMenu();
                 Console.WriteLine("Press key in [] to perform command, Ctrl+Q to exit");
                 keyInfo = Console.ReadKey();
-                while (!ProcessCommand(keyInfo))
+                if (!ProcessCommand(keyInfo))
                 {
+                    Console.Clear();
                     Console.WriteLine("Invalid Command. (Don't press any key modifiers for command)");
-                    keyInfo = Console.ReadKey();
+                    ToMainMenu();
                 }
             }
         }
 
+        public static void ToMainMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press enter to go back to menu");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
         private void LoadCommands()
         {
-            
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(type => type.IsClass && type.Namespace.EndsWith(".Commands"));
+            foreach (var type in types)
+            {
+                var commandObj = Activator.CreateInstance(type) as Command;
+                if (commandObj != null)
+                {
+                    commands.Add(commandObj.InvokeKey, commandObj);
+                }
+            }
+        }
+
+        private void PrintMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("== Menu ==");
+            foreach (var command in commands)
+            {
+                Console.Write("[");
+                Console.Write(command.Key);
+                Console.Write("]");
+                Console.Write(" ");
+                Console.WriteLine(command.Value.ToString());
+            }
+            Console.WriteLine();
         }
 
         private bool ProcessCommand(ConsoleKeyInfo key)
@@ -53,13 +96,19 @@ namespace GoldMine.OperationTool
             if (commands.ContainsKey(key.Key))
             {
                 var command = commands[key.Key];
-                if (!command.Run())
+
+                try
                 {
-                    Console.WriteLine("There were some errors while running command {0}", command.ToString());
+                    Console.Clear();
+                    command.Run();
                 }
-                Console.WriteLine("Press enter to go back to menu");
-                Console.ReadLine();
-                Console.Clear();
+                catch (Exception e)
+                {
+                    Console.Clear();
+                    Console.WriteLine("There were some errors while running command {0}", command.ToString());
+                    Console.WriteLine(e.ToString());
+                    ToMainMenu();
+                }
                 return true;
             }
             else
