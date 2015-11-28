a5ba63299c06cb08bdf4be597ebccf168dd983ee
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2015-11-28 16:53:33 +0900

    OperationTool: AddUser Command Implementation

--- OperationTool/Command.cs
+++ OperationTool/Command.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.Reflection;
 
 namespace GoldMine.OperationTool
 {
     public abstract class Command
     {
         public abstract ConsoleKey InvokeKey { get; }
-        public abstract bool Run();
+
+        public abstract void Run();
+
+        public override string ToString()
+        {
+            return MethodBase.GetCurrentMethod().DeclaringType.Name;
+        }
     }
 }
---
+++ OperationTool/Commands/AddUser.cs
@@ -0,0 +1,48 @@
+﻿using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using GoldMine.DataModel;
+using System;
+
+namespace GoldMine.OperationTool.Commands
+{
+    public class AddUser : Command
+    {
+        public override ConsoleKey InvokeKey
+        {
+            get
+            {
+                return ConsoleKey.A;
+            }
+        }
+
+        public override void Run()
+        {
+            var req = new UpdateItemRequest()
+            {
+                TableName = "counters",
+                UpdateExpression = "ADD id :incr",
+                ReturnValues = new ReturnValue("UPDATED_NEW")
+            };
+            req.Key.Add("table_name", new AttributeValue("user"));
+            req.ExpressionAttributeValues.Add(":incr", new AttributeValue() { N = "1" });
+            req.UpdateExpression = "ADD id :incr";
+
+            var resp = DynamoDBClient.Instance.UpdateItem(req);
+            short newId = short.Parse(resp.Attributes["id"].N);
+
+            using (var ctx = new DynamoDBContext(DynamoDBClient.Instance))
+            {
+                User user = new User()
+                {
+                    id = newId
+                };
+                ctx.Save(user);
+            }
+            Console.WriteLine("User [" + newId + "] has been added");
+
+            if (Program.IsInteractive)
+                Program.ToMainMenu();
+        }
+    }
+}
