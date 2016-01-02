e9a0410ae9d2132dd76ad627022229834a4f73b2
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2016-01-02 23:14:14 +0900

    Attributes for data models to save them into / load them from redis cache: models should use string (hash)key or implement string key to key conversion method

---
+++ DataModel/Attribute/UseStringKeyAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GoldMine.DataModel.Attribute
+{
+    /// <summary>
+    /// Attribute to mark data model having this attribute uses string type as (hash)key
+    /// implying no key type conversion is needed when storing the model into redis
+    /// GetKeyFromStringAttribute will be ignored when this attribute is attached
+    /// </summary>
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
+    public sealed class UseStringKeyAttribute : System.Attribute
+    {
+    }
+}
