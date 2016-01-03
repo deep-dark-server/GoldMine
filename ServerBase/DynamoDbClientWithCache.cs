a4f8f55640f363008f2e154b2c7f340edf0de5ca
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2016-01-03 15:31:45 +0900

    Added singleton of RedisClientWithDynamoDbSync

---
+++ ServerBase/DynamoDbClientWithCache.cs
@@ -0,0 +1,16 @@
+﻿using GoldMine.ServerBase.Init;
+using GoldMine.ServerBase.Redis;
+using GoldMine.ServerBase.Util;
+using System;
+
+namespace GoldMine.ServerBase
+{
+    [PostAppInit]
+    public class DynamoDbClientWithCache : Singleton<RedisClientWithDynamoDbSync>
+    {
+        public static void PostAppInit()
+        {
+            Console.WriteLine("WarmUp: " + Instance.ToString());
+        }
+    }
+}
