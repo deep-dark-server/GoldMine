14eb8dd2aa20dbdc4975f1971589430419850fa6
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2016-01-07 10:57:36 +0900

    PrimeNumberTable to be used for issuing problem

---
+++ MainServer/PrimeNumberTable.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using GoldMine.ServerBase.Init;
+
+namespace GoldMine.MainServer
+{
+    /// <summary>
+    /// Maintains List of prime numbers below int.MAX
+    /// Has some utility methods like PickRandom()
+    /// </summary>
+    [PostAppInit]
+    public class PrimeNumberTable
+    {
+        private static List<int> primeNumbers = new List<int>();
+
+        static PrimeNumberTable()
+        {
+            
+        }   
+    }
+}
