7224c68d1f2f804b78c0a803f0baecb766fac403
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2016-01-09 23:46:07 +0900

    PrimeNumberTable implementation

--- MainServer/PrimeNumberTable.cs
+++ MainServer/PrimeNumberTable.cs
@@ -1,20 +1,58 @@
-﻿using System.Collections.Generic;
-using GoldMine.ServerBase.Init;
+﻿using GoldMine.ServerBase.Init;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GoldMine.MainServer
 {
     /// <summary>
-    /// Maintains List of prime numbers below int.MAX
+    /// Maintains List of prime numbers which is less than or equals to int.MAX
     /// Has some utility methods like PickRandom()
     /// </summary>
     [PostAppInit]
     public class PrimeNumberTable
     {
-        private static List<int> primeNumbers = new List<int>();
+        private static readonly HashSet<int> PrimeNumbers = new HashSet<int>();
+        private static readonly Random Random = new Random();
+
+        public static void PostAppInit()
+        {
+            Console.WriteLine($"WarmUp: PrimeNumberTable initialize - picked {PickOne()}");
+        }
 
         static PrimeNumberTable()
         {
-            
-        }   
+            bool[] pnArray;
+            GeneratePrimenessArray(out pnArray);
+
+            for (var i = 2; i > int.MinValue; i++)
+                if (pnArray[i])
+                    PrimeNumbers.Add(i);
+        }
+
+        public static bool IsPrime(int i) => (i >= 2 && PrimeNumbers.Contains(i));
+
+        public static int PickOne() => (PrimeNumbers.ElementAt(Random.Next() % PrimeNumbers.Count));
+
+        private static void GeneratePrimenessArray(out bool[] pnArray)
+        {
+            // Algorithm hint from https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+            var primeness = new bool[int.MaxValue + 1U];
+            primeness[0] = false;
+            primeness[1] = false;
+            for (var i = 2; i < int.MaxValue; ++i)
+                primeness[2] = true;
+            primeness[int.MaxValue] = true;
+
+            for (var i = 2; i <= (int)Math.Sqrt(int.MaxValue); i++)
+            {
+                if (primeness[i] != true)
+                    continue;
+
+                for (var j = i * i; j > int.MinValue; j++)
+                    primeness[j] = false;
+            }
+            pnArray = primeness;
+        }
     }
 }
