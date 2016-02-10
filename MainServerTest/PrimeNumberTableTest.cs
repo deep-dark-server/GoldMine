bf14e68a925cebf7101bd269f436eaacbabe1c88
LemonTeaDev <LemonTeaDev@users.noreply.github.com>
2016-02-10 15:33:34 +0900

    Added NUnit Test

---
+++ MainServerTest/PrimeNumberTableTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace GoldMine.MainServer.Test
+{
+    [TestFixture]
+    public class PrimeNumberTableTest
+    {
+        //[Test]
+        /// <summary>
+        /// This test uses very inefficient method to check validity, 
+        /// so pass this test when there was no major modifications.
+        /// </summary>
+        public void IsPrimeNumbersValid()
+        {
+            Assert.Equals(true, PrimeNumberTable.IsPrimeNumberSetValid);
+        }
+
+        /// <summary>
+        /// Testing IsPrime() funcionality - pre-generated prime number table lookup
+        /// </summary>
+        /// <param name="n"></param>
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(7)]
+        [TestCase(11)]
+        public void IsPrime(int n)
+        {
+            Assert.AreEqual(true, PrimeNumberTable.IsPrime(n));
+        }
+    }
+}
